8b93d889c47721e41f9b867bac7791dcc5a44f08
João Pedro Giarrante <joao.giarrante@valemobi.com.br>
2020-07-08 02:00:01 -0300

    ...

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var imgBall: UIImageView!
+    @IBOutlet weak var btnAsk: UIButton!
+    
+    override func viewDidLoad() {
+        self.btnAsk.layer.cornerRadius = 20;
+    }
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBAction func btnAsk(_ sender: UIButton) {
+        imgBall.image = ballArray.randomElement();
+    }
+    
+    override func motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if(motion == UIEvent.EventSubtype.motionShake){
+            self .btnAsk(btnAsk)
+        }
+    }
+    
 }
 
